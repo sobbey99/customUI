a63dd6ad1353d0e7e9336dd278f2506737ea4ae2
sobbey99 <sobbey99@gmail.com>
2023-01-19 10:46:26 +0200

    Methods to work with elements ( CLOSEST, SIBLINGS )

--- dist/script.js
+++ dist/script.js
@@ -118,6 +118,40 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   }
   return this;
 };
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
+  const objLength = Object.keys(this).length;
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+  return this;
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+      this[counter] = arr[j];
+      counter++;
+    }
+    numberOfItems += arr.length - 1;
+  }
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+  return this;
+};
 
 /***/ }),
 
@@ -321,6 +355,8 @@ function sayHello() {
   console.log((0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])(this).index());
 });
 console.log((0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(2).find('.some'));
+console.log((0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.some').closest('.findme'));
+console.log((0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.findme').siblings());
 })();
 
 /******/ })()
--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,7 @@ $('div').click(function() {
 });
 
 console.log($('div').eq(2).find('.some'));
+
+console.log($('.some').closest('.findme'));
+
+console.log($('.findme').siblings());
